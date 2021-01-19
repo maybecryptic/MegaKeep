b76965bf630765a6d6bc65a10cb095ce458fddda
CrazyOldWizard <CrazyOldWizard@users.noreply.github.com>
2021-01-18 20:56:51 -0600

    Added ability to run via command line with arguments "--cli" and can set file path with "--txtFile" #5 (#6)

--- MegaKeep/Program.cs
+++ MegaKeep/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Windows.Forms;
 
 namespace MegaKeep
@@ -9,11 +10,31 @@ namespace MegaKeep
 		/// The main entry point for the application.
 		/// </summary>
 		[STAThread]
-		static void Main()
+		static void Main(string[] args)
 		{
 			Application.EnableVisualStyles();
 			Application.SetCompatibleTextRenderingDefault(false);
-			Application.Run(new MegaKeep());
+			CommandArgs(args);
+			Application.Run(new MegaKeep(args)); //passes args to new start method
+		}
+
+		// sets the path to the text file passed via command line.
+		static void CommandArgs(string[] args)
+		{
+			if (args.Length > 0)
+			{
+				var txtFileIndex = Array.IndexOf(args, "--txtFile") + 1;
+				if(args[txtFileIndex] != null)
+				{
+					var txtFile = args[txtFileIndex];
+					if (File.Exists(txtFile))
+					{
+						Properties.Settings.Default.Location = txtFile;
+						Properties.Settings.Default.Save();
+					}
+				}
+				
+			}
 		}
 	}
 }
